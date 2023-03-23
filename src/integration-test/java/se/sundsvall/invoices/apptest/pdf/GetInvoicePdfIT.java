3c5bed0f46ca1846527df56f71f365bf90cda36e
Joakim Schürer <joakim.schurer@sundsvall.se>
2023-03-23 09:31:33 +0100

    UF-4671: Addition of IT tests for pdf resource

---
+++ src/integration-test/java/se/sundsvall/invoices/apptest/pdf/GetInvoicePdfIT.java
@@ -0,0 +1,44 @@
+package se.sundsvall.invoices.apptest.pdf;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpStatus.BAD_GATEWAY;
+import static org.springframework.http.HttpStatus.OK;
+
+import org.junit.jupiter.api.Test;
+
+import se.sundsvall.dept44.test.AbstractAppTest;
+import se.sundsvall.dept44.test.annotation.wiremock.WireMockAppTestSuite;
+import se.sundsvall.invoices.Application;
+
+@WireMockAppTestSuite(files = "classpath:/GetInvoicePdf/", classes = Application.class)
+class GetInvoicePdfIT extends AbstractAppTest {
+	private static final String PATH_PREFIX = "/COMMERCIAL/";
+	private static final String PATH_SUFFIX = "/pdf";
+	private static final String RESPONSE_FILE = "response.json";
+
+	@Test
+	void test01_getInvoicePdf() {
+		final var organizationNumber = "5565257545";
+		final var invoiceNumber = "111222";
+
+		setupCall()
+			.withServicePath(PATH_PREFIX + organizationNumber + "/" + invoiceNumber + PATH_SUFFIX)
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
+	}
+
+	@Test
+	void test02_getInvoicePdfNotFound() {
+		final var organizationNumber = "5565257545";
+		final var invoiceNumber = "666";
+
+		setupCall()
+			.withServicePath(PATH_PREFIX + organizationNumber + "/" + invoiceNumber + PATH_SUFFIX)
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(BAD_GATEWAY)
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
+	}
+}
