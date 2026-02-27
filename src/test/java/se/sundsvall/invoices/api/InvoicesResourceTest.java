4f3f78f5f12164a701f46491b7e5ef8e8a487f39
Linus Sjölinder <linus.sjolinder@b3.se>
2026-02-27 15:56:40 +0100

    UF-18143: update to dept44 8.0.0 and SB4

--- src/test/java/se/sundsvall/invoices/api/InvoicesResourceTest.java
+++ src/test/java/se/sundsvall/invoices/api/InvoicesResourceTest.java
@@ -4,12 +4,15 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -39,6 +42,8 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static se.sundsvall.invoices.api.model.InvoiceOrigin.COMMERCIAL;
 
+@AutoConfigureWebTestClient
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest(classes = Application.class, webEnvironment = RANDOM_PORT)
 @ActiveProfiles("junit")
 class InvoicesResourceTest {
